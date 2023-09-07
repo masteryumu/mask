3ec6e5a49acd1756601ee78e4ef2ffe9c9f4187d
成浩 邱 <1320460765@qq.com>
2023-09-07 18:42:49 +0800

    retry

--- api/proxy.mjs
+++ api/proxy.mjs
@@ -1,17 +1,12 @@
 import express from 'express';
 import axios from 'axios';
-import cors from 'cors';
-import bodyParser from 'body-parser'; // 引入body-parser模块
+import cors from 'cors'; // 引入cors模块
 
 const app = express();
 const port = 3000;
 
+// 使用cors中间件，允许所有源访问代理服务器
 app.use(cors());
-app.use(bodyParser.json()); // 使用body-parser来解析JSON请求体
-
-// 在代理服务器代码中添加这行，将静态文件目录设置为存放图像的目录
-app.use(express.static('E:/Desktop/mask/api'));
-
 
 // 处理根路径的请求
 app.get('/', (req, res) => {
@@ -39,34 +34,67 @@ app.post('/mergeImages', async (req, res) => {
   try {
     const requestBody = req.body; // 获取请求体作为 JSON 对象
 
-    // 现在，你可以从请求体中获取 base64 图像数据
-    const base64ImageTarget = requestBody.image_target;
-    const base64ImageTemplate = requestBody.image_template;
-
     // 在这里设置图像合成的参数
     const image_target = {
-      image: base64ImageTarget,
-      image_type: "BASE64",
+      image: 'https://img0.baidu.com/it/u=788673368,4264362421&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=463',
+      image_type: "URL",
       quality_control: "NONE"
     };
     
     const image_template = {
-      image: base64ImageTemplate,
-      image_type: "BASE64"
+      image: 'https://img0.baidu.com/it/u=2280817350,814068723&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
+      image_type: "URL"
     };
 
-    const version = "2.0";
+        const version = "2.0";
+        // 添加其他参数如 alpha 和 merge_degree，如果需要
 
-    // 在这里使用设置的参数进行图像合成操作，例如调用相应的图像合成 API
-    // 示例：这里假设你调用了某个图像合成 API，并将结果保存在 response 中
-    const response = await performImageMerge(image_target, image_template, version);
+        // 在这里使用设置的参数进行图像合成操作，例如调用相应的图像合成 API
+        // 示例：这里假设你调用了某个图像合成 API，并将结果保存在 response 中
+        const response = await performImageMerge(image_target, image_template, version);
 
-    // 返回图像合成的结果
-    res.json(response);
-  } catch (error) {
-    res.status(500).json({ error: '图像合成失败' });
-  }
+        // 返回图像合成的结果
+        res.json(response);
+    } catch (error) {
+        res.status(500).json({ error: '图像合成失败' });
+    }
 });
+
+async function performImageMerge(imageTarget, imageTemplate, version) {
+  try {
+    // 在这里执行图像合成操作，调用百度AI服务或其他图像处理库
+    // 这里需要根据你的具体需求编写代码，将imageTarget和imageTemplate合成并返回结果
+    var options = {
+      'method': 'POST',
+      'url': url,
+      'headers': {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "image_target": {
+          "image": imageTarget.image,
+          "image_type": "URL",
+          "quality_control": "NONE"
+        },
+        "image_template": {
+          "image": imageTemplate.image,
+          "image_type": "URL"
+        },
+        "version": version,
+                
+      })
+
+    };
+
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+      console.log(response.body);
+    });
+    
+  }
+}
+
+
 
 app.listen(port, () => {
   console.log(`代理服务器已启动，监听端口 ${port}`);
